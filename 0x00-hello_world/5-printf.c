4216dadc8759cfcec9bd9852a61f98166bdbcdbd
spindouken <mlc9181209@gmail.com>
2022-09-16 13:43:06 -0500

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - this is my first c put
+ * main - this is my first c printf
+ *
+ * Description: Betty bois
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf(with proper grammar, but the outcome is a piece of art,\n);
-    return (0);
+    printf("with proper grammar, but the outcome is a piece of art,\n");
 }
