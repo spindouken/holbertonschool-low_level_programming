0c9bb197f93c5cb3e471b62aff7c09a642dda414
spindouken <mlc9181209@gmail.com>
2022-09-16 12:15:58 -0500

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this is my first c put
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf(with proper grammar, but the outcome is a piece of art,\n);
+    return (0);
+}
