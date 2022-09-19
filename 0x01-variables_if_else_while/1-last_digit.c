fa2c7128a0bf72c61deef308c1e2351809276b91
spindouken <spindouken@gmail.com>
2022-09-18 22:54:56 -0500

    spaces

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,22 +10,22 @@
  */
 int main(void)
 {
-    int n;
+	int n;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    printf("last digit of %i is %i ", n, n % 10);
-    if (n % 10 > 5)
-    {
-        printf("%i and is greater than 5\n");
-    }
-    else if (n % 10 == 0)
-    {
-        printf("%i and is 0\n", n);
-    }
-    else if (n % 10 > 6 && n % 10 != 0)
-    {
-        printf("%i and is less than 6 and not 0\n");
-    }
-    return (0);
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("last digit of %i is %i ", n, n % 10);
+	if (n % 10 > 5)
+	{
+		printf("%i and is greater than 5\n");
+	}
+	else if (n % 10 == 0)
+	{
+		printf("%i and is 0\n", n);
+	}
+	else if (n % 10 > 6 && n % 10 != 0)
+	{
+		printf("%i and is less than 6 and not 0\n");
+	}
+	return (0);
 }
