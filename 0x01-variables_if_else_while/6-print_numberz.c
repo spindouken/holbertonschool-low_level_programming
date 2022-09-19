e63bc195b4f0b4df17f8ded5a34c023a4c9799a5
spindouken <mlc9181209@gmail.com>
2022-09-19 13:48:19 -0500

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-10 with putchar
+ *
+ * Description: Betty bois
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar ("%d", x);
+	}
+	putchar("\n");
+	return (0);
+}
