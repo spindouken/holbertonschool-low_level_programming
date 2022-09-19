143e06f9f219fd3e41adb84d8dba6e0e3db1530d
spindouken <mlc9181209@gmail.com>
2022-09-19 14:48:29 -0500

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - putchar lower case all alphabet
+ *
+ * Description: Betty bois
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char x;
+	
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
