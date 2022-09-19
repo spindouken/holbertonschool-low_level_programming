5e306f8271af2d4fd9d38ce5e5eb7c7b32164b18
spindouken <mlc9181209@gmail.com>
2022-09-19 10:32:15 -0500

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print last digit of random number generated
+ *
+ * Description: Betty bois
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for(int x = 'A'; x <= 'Z'; x++)
+	{
+	putchar(tolower(x));
+	putchar(small);
+	}
+	return (0);	
+}
+
