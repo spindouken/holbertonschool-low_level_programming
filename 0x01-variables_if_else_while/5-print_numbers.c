1ca831f1e228dc90ccb0d74916886fbe658af70b
spindouken <mlc9181209@gmail.com>
2022-09-19 11:58:25 -0500

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-10
+ *
+ * Description: Betty bois
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		printf(x)
+	}
+	printf('\n');
+	return (0);
+}
