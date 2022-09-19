39ffaaa443d097cbc7419be8711ccf2c9188326a
spindouken <spindouken@gmail.com>
2022-09-19 18:45:51 -0500

    mega push plus q 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ *
+ * Description
+ *
+ * Return: On success 0.
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
