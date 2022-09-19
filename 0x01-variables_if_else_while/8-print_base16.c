a8b26c0601c1745c5394ad499f395f3de0de0390
spindouken <spindouken@gmail.com>
2022-09-19 16:30:55 -0500

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print all the numbers of base 16 in lowercase
+ *
+ * Description: Betty bois
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int asciiarray[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+	int x;
+
+	for (x = 0; x < 16; x++)
+	{
+		putchar(asciiarray[x]);
+	}
+	putchar('\n');
+	return (0);
+}
