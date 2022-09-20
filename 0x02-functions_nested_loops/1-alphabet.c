4702fb6884dde85073181c5efbe7c6f4f89290d3
spindouken <spindouken@gmail.com>
2022-09-19 19:45:31 -0500

    prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet 
+ *
+ * Description: print all of the lower case alphabet
+ *
+ * Return: On success 0.
+ */
+void print_alphabet(void);
+{
+	char x;
+	
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n')
+	return (0);
+}
