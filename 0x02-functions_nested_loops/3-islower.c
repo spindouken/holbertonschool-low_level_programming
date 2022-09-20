584401fc864713f12df7297670601c2a2d41053d
spindouken <spindouken@gmail.com>
2022-09-19 20:44:30 -0500

    starting 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * int _islower(int c) - checking if c is lowercase
+ *
+ * Description: return 1 if c is lowercase
+ *
+ * Return: 1 for lowercase c elsewise 0
+ */
+int _islower(int c)
+{
+
+	if (c = 99)
+	{
+	return (1)
+	}
+	return (0)
+}
