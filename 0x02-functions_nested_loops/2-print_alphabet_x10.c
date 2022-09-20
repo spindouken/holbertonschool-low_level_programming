15d03c687be0eb65eee6fd57dd6d9a66a0eb40b9
spindouken <spindouken@gmail.com>
2022-09-19 20:24:17 -0500

    maybe 2 ez?

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet - making function that prints the alphabet
+ *
+ * Description: print all of the lower case alphabet
+ *
+ * Return: On success 0.
+ */
+void print_alphabet_x10(void)
+{
+	char x;
+
+	while (i = 0, i <= 9, i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+	_putchar('\n');
+	}
+}
