f1728360f080542c0ed0a15bcf6beb82b443886f
spindouken <spindouken@gmail.com>
2022-09-19 21:52:13 -0500

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isalpha - checking if variable is an alphabetic character
+ * @c: variable
+ * Description: return 1 if variable is alphabetic character
+ *
+ * Return: 1 if c is an alphabetic character elsewise 0
+ */
+int _isalpha(int c)
+{
+
+	if (c <= 65 && c >= 90) || (c <= 122 && c >= 97)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
