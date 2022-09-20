99aab52ecbce5e5265543386b52c99bc7148b0a9
spindouken <spindouken@gmail.com>
2022-09-19 22:20:15 -0500

    betty bois

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,14 +14,15 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else (n < 0)
+	else if (n < 0)
 	{
 		_putchar('-');
 		return (-1);
 	}
-	else if (n == 0)
+	else
 	{
 		_putchar('0');
-		return (-1);
+		return (0);
 	}
+
 }
