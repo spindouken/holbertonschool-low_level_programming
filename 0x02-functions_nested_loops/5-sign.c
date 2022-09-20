a8fa85e7a96b0e65aea7075cabaeafd0831649f9
spindouken <spindouken@gmail.com>
2022-09-19 22:11:25 -0500

    first try right?

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - print if the variable is negative positive or zero
+ * @c: variable
+ * Description: return the sign of the variable
+ *
+ * Return: 1 if n is greater than zero, 0 if zero, and -1 if less than zero
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (-1);
+	}
+}
