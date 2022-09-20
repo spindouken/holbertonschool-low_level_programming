d0d6379e23a97e30c00ad87df0e733f39e44f000
spindouken <spindouken@gmail.com>
2022-09-19 23:45:00 -0500

    10 first try?

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - addition of variables
+ * @n1: variable 1 (integer)
+ * @n2: variable 2 (integer)
+ * Description: print the sum of the addition of two integers
+ *
+ * Return: integer value after addition
+ */
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
