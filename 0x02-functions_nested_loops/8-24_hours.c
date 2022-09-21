fc6ec9317d334f1bae5f811921c54607981038fc
spindouken <mlc9181209@gmail.com>
2022-09-21 16:09:11 -0500

    7

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * jack_bauer - 
+ * @r: variable
+ * Description: print the last digit of the variable
+ *
+ * Return: 0 always
+ *
+ */
+int print_last_digit(int r)
+{
+	int i
+
+	(r % 10 = i);
+	_putchar(i);
+	return (0);
+}
