93b624c4a062c7f103b5572546c4e1843155bdd4
spindouken <spindouken@gmail.com>
2022-09-21 18:42:47 -0500

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,21 +1,23 @@
 #include "main.h"
 /**
- * jack_bauer - print every minute of the day
- * @r: variable
- * Description: print every minute of the day
- *
- * Return: 0 always
- *
+ * jack_bauer - print every minute and hour of the day (clock form)
+ * Description: print every minute and hour of the day (clock form)
+ * Return: all valid
  */
 void jack_bauer(void)
 {
 	int hour, minute;
 
-	for (hour = 0, hour <= 23, hour++)
+	for (hour = 0; hour <= 23; hour++)
 	{
-		for (minute = 0; minute <= 59, minute++)
+		for (minute = 0; minute <= 59; minute++)
 		{
-			_putchar
+			_putchar((hour/10)+48);
+			_putchar((hour % 10)+48);
+			_putchar(':');
+			_putchar((minute/10)+48);
+			_putchar((minute % 10)+48);
+			_putchar('\n');
 		}
 	}
 }
