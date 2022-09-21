a90e9c0bbf704a2e8452e426db27d1aea011dd6e
spindouken <mlc9181209@gmail.com>
2022-09-21 15:28:13 -0500

    7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @r: variable
+ * Description: print the last digit of the variable
+ *
+ * Return: 0 always
+ *
+ */
+int print_last_digit(int r)
+{
+	i = int
+	r % 10 = i
+
+	putchar(i);
+	return (0);
+}
