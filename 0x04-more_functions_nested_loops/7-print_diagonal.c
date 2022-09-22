540e3abc4b69b83a2d502b61e140e6f7f43ea3d2
spindouken <mlc9181209@gmail.com>
2022-09-22 13:45:26 -0500

    send it

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,16 @@
  * Return: \
  */
 void print_diagonal(int n)
-
 {
 	int x;
 
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 	for (x = 0; x < n; x++)
 	{
 		_putchar(92);
 		_putchar('\n');
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
 	}
 }
