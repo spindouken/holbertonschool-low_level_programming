3b514adf9ce63d2195d51626093bebfbc201af15
spindouken <mlc9181209@gmail.com>
2022-09-22 10:51:13 -0500

    4 betty

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,7 +6,7 @@
  */
 void print_most_numbers(void)
 {
-	int x=0;
+	int x = 0;
 
 	for (x = 0; x <= 9; x++)
 	{
@@ -14,7 +14,7 @@ void print_most_numbers(void)
 		{
 			_putchar(x + 48);
 		}
-		
+
 		_putchar('\n');
 	}
 }
