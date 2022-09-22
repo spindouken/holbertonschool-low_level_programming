66b1391ce73d6123d17ada01861a43ec8f9925ce
spindouken <mlc9181209@gmail.com>
2022-09-22 10:43:29 -0500

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		if (!(x == 2 && x == 4))
+		{
+			_putchar(x+48)
+		}
+	_putchar('\n');
+}
