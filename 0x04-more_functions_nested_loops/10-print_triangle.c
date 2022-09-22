7d2d6611f126a23deb13aab9a3530c8613df9631
spindouken <mlc9181209@gmail.com>
2022-09-22 15:14:28 -0500

    8

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_triangle - generate triangle extendable through variable
+ *
+ * @n: variable
+ *
+ * Return: 
+ */
+void print_triangle(int size)
+{
+	int x;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (x = 0; x < n; x++)
+	{
+		_putchar(92);
+		_putchar('\n');
+	}
+}
