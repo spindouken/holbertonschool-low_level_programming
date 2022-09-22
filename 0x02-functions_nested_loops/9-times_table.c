347dd0ccd18ea01a16ad9fe8a6dd5fbbecebb53a
spindouken <spindouken@gmail.com>
2022-09-21 21:34:13 -0500

    aljfdlaksjf

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,6 +22,7 @@ void times_table(void)
 					_putchar((product % 10) + 48);
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 				}
 				else
 				{
@@ -35,7 +36,6 @@ void times_table(void)
 			{
 				if (product <= 9)
 				{
-					_putchar(' ');
 					_putchar((product % 10) + 48);
 				}
 				else
