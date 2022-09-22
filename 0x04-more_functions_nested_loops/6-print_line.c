962f123ca2ff6c90d84dab29892d2c35cfe23392
spindouken <mlc9181209@gmail.com>
2022-09-22 13:18:27 -0500

    pretty straightforward

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - generate straight line extendable through variable
+ *
+ * @n: variable
+ *
+ * Return: _
+ */
+void print_line(int n);
+{
+	int x;
+
+	for (x = 0; x < n; x++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
+}
