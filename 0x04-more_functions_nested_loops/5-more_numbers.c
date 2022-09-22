530d975b9f0539f633efbf4c366d1e1595672f42
spindouken <mlc9181209@gmail.com>
2022-09-22 12:01:59 -0500

    retry

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@ void more_numbers(void)
 	int x;
 	int y;
 
-	for (x = 0; x <= 10; x++)
+	for (x = 0; x < 10; x++)
 	{
 		for (y = 0; y <= 14; y++)
 		{
 			if (y >= 10)
 			{
-				_putchar(y / 10 + '0');
+				_putchar((y / 10 +) 48);
 			}
-			_putchar(y % 10 + '0');
+			_putchar((y % 10) + 48);
 		}
 	}
 	_putchar('\n');
