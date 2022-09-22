0dc433621f598bdfca78136384fdacf3294295ab
spindouken <spindouken@gmail.com>
2022-09-21 20:31:44 -0500

    9 form start

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * times_table - indicative of times table of variable
+ * Description: print times tables of 0-9
+ * Return: void
+ */
+void times_table(void)
+{
+	int number;
+	int multiplier;
+	int equals;
+
+	for (number = 0; number <= 8; number++)
+	{
+		_putchar(number+48)
+		for (multiplier = 1; multiplier <= 8; multiplier++)
+		{
+			number * mulitplier = equals;
+		}
+		_putchar('\n');
+	}
+}
