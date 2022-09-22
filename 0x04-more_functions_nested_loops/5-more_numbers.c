29ba54d31b3de0a9f5dcbb997c03e430f1bea331
spindouken <mlc9181209@gmail.com>
2022-09-22 11:40:02 -0500

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * more_numbers - we're going to multiply
+ *
+ * Return: void (infinite possible returns)
+ */
+void more_numbers(void)
+{
+	int x;
+	int y;
+
+	for (x = 0; x <= 14; x++)
+	{
+		if (y = 0; y <= 10; y++)
+		(return = x * y)
+	}
+	_putchar(return + 48)
+	_putchar('\n');
+}
