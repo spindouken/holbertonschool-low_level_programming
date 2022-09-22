c8d162c88a313ae5e2edde547c3de622166c718c
spindouken <mlc9181209@gmail.com>
2022-09-22 10:06:51 -0500

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - used to multiply integers
+ * @a: variable
+ * @b: variable
+ * Return: 1 if character is a digit, 0 otherwise
+ */
+int mul(int a, int b)
+{
+    return (a * b)
+}
