23da1a016e7473bba7bd708f49bc2ac4f70b59c3
spindouken <mlc9181209@gmail.com>
2022-09-23 14:55:14 -0500

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - print string to stdout
+ *
+ * Return: void
+ */
+void _puts(char *str);
+{
+	int x = 0;
+
+	while ((*(str + x)) && (*(str + x)) != '\0')
+		{
+			_putchar(*(str + x));
+			x++;
+		}
+	_putchar('\n');	
+}
