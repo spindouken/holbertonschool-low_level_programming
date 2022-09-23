7aa4be9a6e0f30042ac8d8a2d9a68d761f1ca489
spindouken <mlc9181209@gmail.com>
2022-09-23 11:07:38 -0500

    1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int - swap value of two integers
+ *
+ * @a: variable
+ * @b: variable
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int = value;
+	value = a*;
+	a* = b*;
+	b* = value;
+}
