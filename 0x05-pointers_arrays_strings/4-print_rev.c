c8455e4a0641b02c57f1d9ba65718af61759b76a
spindouken <spindouken@gmail.com>
2022-09-25 13:32:00 -0500

    worth a try

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_rev - print full string in reverse
+ * @s: target string
+ * Return: void
+ */
+void print_rev(char *s)
+{
+    char s[100]= {0}, Temp = 0;
+    int x = 0, z = 0;
+
+    while(s[z++] != '\0');
+
+	z--;
+
+	z--;
+
+	while (x < z)
+	{
+		Temp = s[x];
+		s[x] = s[z];
+		s[z] = Temp;
+		x++;
+		z--;
+		_putchar(s[z]);
+	}
+	_putchar("\n");
+	return 0;
+}
