0b95ebae87abe4f67a978521a74c3cc7d3434e3d
spindouken <spindouken@gmail.com>
2022-09-24 23:25:29 -0500

    maybe betty compliant?

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _puts - print full string
- *@str: target string
+ * @str: target string
  * Return: void
  */
 void _puts(char *str)
@@ -9,6 +9,7 @@ void _puts(char *str)
 	int x;
 
 	x = 0;
+
 	while (str[x] != '\0')
 		{
 			_putchar(str[x]);
