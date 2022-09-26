fb94bb0a30df85272136d18e015a2661a2bf8da7
spindouken <mlc9181209@gmail.com>
2022-09-26 11:03:57 -0500

    def not going to work :)

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of a string
+ * @n: variable that indicates the amount of array elements to be printed from first to last
+ * @a: variable for place in array 
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int *a = 0;
+
+	arr[1000];
+
+	while (*a <= n)
+	{
+		a++;
+		_putchar(48 + *a);
+	}
+
+	_putchar('\n');
+}
