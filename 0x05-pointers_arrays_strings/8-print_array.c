55894ccebc5015781f5ace99ca06b08b2c8fe0ae
spindouken <mlc9181209@gmail.com>
2022-09-26 11:42:21 -0500

    betty no like extra variable desc

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -3,7 +3,6 @@
  * print_array - print n elements of array from first to last
  * @n: indicates amount of array elements to be printed from first to  last
  * @a: array placeholder
- * @x: variable for place in array
  * Return: void
  */
 void print_array(int *a, int n)
