7f943b9584ab65049e27d00b39083b954dcfc9f7
spindouken <spindouken@gmail.com>
2022-09-25 23:21:41 -0500

    modulus style

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,18 +7,15 @@
 void puts2(char *str)
 {
 	int x = 0;
-	int z;
 
 	while (str[x] != '\0')
 	{
 		(x ++);
 	}
 
-	for (z = 0; z < x; z += 2)
+	if (x % 2 == 0)
 	{
-		_putchar(str[z]);
-		str[z/2]=str[z];
-	}
-	_putchar (str[z]);
+		_putchar(str[x]);
+	}	
 	_putchar('\n');
 }
