a2e4292c24e1eccac8a1a0130d709237f27eda61
spindouken <mlc9181209@gmail.com>
2022-09-26 09:30:22 -0500

    betty

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,11 +11,15 @@ void puts_half(char *str)
 	for (x = 0; str[x] != '\0'; x++)
 	{
 		if (x % 2 == 0)
+		{
 			z = (x / 2);
 			_putchar(str[z]);
+		}
 		if (x % 2 == 1 || x == 1)
+		{
 			n = (x - 1) / 2;
 			_putchar(str[n]);
+		}
 	}
 	_putchar('\n');
 }
