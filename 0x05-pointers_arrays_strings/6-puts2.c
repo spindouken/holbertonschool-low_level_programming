6b35d986295e65dc9f833c479bca1d7558ab7587
spindouken <spindouken@gmail.com>
2022-09-25 23:24:28 -0500

    reformat

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,14 +8,10 @@ void puts2(char *str)
 {
 	int x = 0;
 
-	while (str[x] != '\0')
+	while (x = 0; str[x] != '\0'; x++)
 	{
-		(x ++);
+		if (x % 2 == 0)
+			_putchar(str[x]);
 	}
-
-	if (x % 2 == 0)
-	{
-		_putchar(str[x]);
-	}	
 	_putchar('\n');
 }
