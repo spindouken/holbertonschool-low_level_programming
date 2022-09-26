9920fcddee4f0ad1edc5498116f43445f95c4000
spindouken <mlc9181209@gmail.com>
2022-09-26 09:27:49 -0500

    7 first try?

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts_half - print the second half of a string
+ * @str: target string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int x, z, n;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (x % 2 == 0)
+			z = (x / 2);
+			_putchar(str[z]);
+		if (x % 2 == 1 || x == 1)
+			n = (x - 1) / 2;
+			_putchar(str[n]);
+	}
+	_putchar('\n');
+}
