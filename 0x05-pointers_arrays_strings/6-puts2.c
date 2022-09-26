d463af3fc1be6f28c27be398346b25e4039d815c
spindouken <spindouken@gmail.com>
2022-09-25 22:51:58 -0500

    first try?

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - print first character then every other character
+ * @str: target string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int x = 1;
+
+	while (str[x] != '\0')
+	{
+		_putchar(str[x]);
+		(x + 2);
+		// need plus two
+	}
+	_putchar('\n');
+}
