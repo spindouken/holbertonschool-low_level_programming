9afd97c583d6e2835798f6bae93c300729895205
spindouken <spindouken@gmail.com>
2022-09-26 21:11:46 -0500

    retry

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -16,11 +16,11 @@ int main(void)
 		{
 			printf("FizzBuzz");
 		}
-		else if (x % 5 == 0)
+		if (x % 5 == 0)
 		{
 			printf("Buzz");
 		}
-		else if (x % 3 == 0)
+		if (x % 3 == 0)
 		{
 			printf("Fizz");
 		}
