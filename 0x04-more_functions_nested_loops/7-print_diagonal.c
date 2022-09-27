8680ab057a467bba8be13423e56b4572d30b2b95
spindouken <spindouken@gmail.com>
2022-09-27 18:00:33 -0500

    attempt

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,15 +8,18 @@
  */
 void print_diagonal(int n)
 {
-	int x;
+int x, z;
 
-	if (n <= 0)
-	{
+	if (n > 0)
+		while (x = 1; n >= x; x++)
+		{
+			while (z = 1; x > z; z++)
+				_putchar(32);
+			_putchar(92);
+			_putchar(92);
+			_putchar('\n');
+		}
+
+	if (n < 1)
 		_putchar('\n');
-	}
-	for (x = 0; x < n; x++)
-	{
-		_putchar(92);
-		_putchar('\n');
-	}
 }
