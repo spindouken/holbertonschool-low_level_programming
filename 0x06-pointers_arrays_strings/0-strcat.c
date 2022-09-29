2eac2a5223c14b15fcdaf64d49a18829f1972c5f
spindouken <spindouken@gmail.com>
2022-09-29 14:45:27 -0500

    start

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,9 +1,19 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @
- * Return: 
+ * _strcat - concatenate strings
+ * @src:
+ * @dest:
+ * Return: point to the string destination
  */
 
 char *_strcat(char *dest, char *src)
+{
+	int x, y;
+
+	while (x = 0; dest[x] != '\0'; x++)
+
+	while (y = 0; src[y] != '\0'; x++, y++)
+
+	return (dest);
+}
