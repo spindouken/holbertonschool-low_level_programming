eed1f0cd60dfadccc824193ec7b5a9fe1b85a5dc
spindouken <spindouken@gmail.com>
2022-09-30 16:40:45 -0500

    unknown compile error, trying brackets

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,11 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 	int x;
 
 	for (x = 0; src[x] != '\0' && x < n; x++)
+	{
 		dest[x] = src[x];
-
+	}
 	while (x < n)
+	{
 		dest[x] = '\0';
 		x++;
-
+	}
 	return (dest);
 }
