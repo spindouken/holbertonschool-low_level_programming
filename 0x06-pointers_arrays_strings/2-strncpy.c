aea71a6fa6f4aab48f146613cffb107e7ddb0a67
spindouken <spindouken@gmail.com>
2022-09-30 16:21:03 -0500

    proto

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,9 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @
- * Return: 
+ * _strncpy - copy a string
+ * @dest: destination string
+ * @src: source string
+ * @n: variable
+ * Return: the copied string
  */
 
 char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+    for (x = 0; s1[x] != '\0'; ++x)
+        s2[x] = s1[x];
+
+    s2[x] = '\0';
+    
+    return (dest);
+}
