50d886e9eaf3c626af3394c67d85f11d0b707611
spindouken <spindouken@gmail.com>
2022-09-30 17:59:38 -0500

    def not right

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,8 +9,6 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int x = 0;
-
 	while (*s1 != '\0' || *s2 != '\0')
 	{
 		if (*s1 == *s2)
@@ -21,9 +19,6 @@ int _strcmp(char *s1, char *s2)
 		else if ((*s1 == '\0' && *s2 != '\0')
 				|| (*s1 != '\0' && *s2 == '\0')
 				|| (*s1 != *s2))
-		{
-			x = 1;
-		}
 	}
 	return (0);
 }
