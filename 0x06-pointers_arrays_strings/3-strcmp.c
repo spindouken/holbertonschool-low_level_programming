2ddf9849994820cc0cec4694ac905f5c90e0cbb3
spindouken <spindouken@gmail.com>
2022-09-30 17:53:23 -0500

    want to try model

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,30 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @
- * Return: 
+ * _strcmp - compare two strings
+ * @s1: string one
+ * @s2: string two
+ * Return: 0 on success
  */
 
 int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+
+	while (*s1 != '\0' || *s2 != '\0')
+	{
+		if (*s1 == *s2)
+		{
+			s1++;
+			s2++;
+		}
+		else if ((*s1 == '\0' && *s2 != '\0')
+		{
+				|| (*s1 != '\0' && *s2 == '\0')
+				|| (*s1 != *s2)
+			x = 0;
+		}
+	}
+	if (x == 0)
+	return (0);
+}
