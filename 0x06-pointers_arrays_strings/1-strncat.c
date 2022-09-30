2a845770848dabbdc189171e30104eca3b954b34
spindouken <spindouken@gmail.com>
2022-09-30 16:19:58 -0500

    proto copy

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,3 +7,21 @@
  */
 
 char *_strncat(char *dest, char *src, int n)
+{
+	int x = 0;
+	int y = 0;
+
+	while (dest[x] != '\0')
+		x++;
+
+	while (src[y] != src[n])
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+
+	dest[x] = '\0';
+
+	return (dest);
+}
