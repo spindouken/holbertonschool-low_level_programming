81d55aaa97605546df7b41e6c5af87d6484ff963
spindouken <spindouken@gmail.com>
2022-10-01 12:22:19 -0500

    big proto push

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -7,3 +7,26 @@
  */
 
 void reverse_array(int *a, int n)
+x = 0;
+
+{
+	int x = 0;
+	int z = 0;
+	int tmp;
+
+	while (s[x] != '\0')
+	{
+		x++;
+	}
+
+	x--;
+
+	while (z < x)
+	{
+		tmp = s[x];
+		s[x] = s[z];
+		s[z] = tmp;
+		z++;
+		x--;
+	}
+}
