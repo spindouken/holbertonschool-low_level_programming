2cd6a116a5a2720e99f7964fb6f4fae38aa654a9
spindouken <spindouken@gmail.com>
2022-10-01 15:22:21 -0500

    exactly 80 char

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _memset -  fill first n bytes of the memory area at pointer s w/ const byte b
+ * _memset - fill first n bytes of the memory area at pointer s w/ const byte b
  * @s: memory area
  * @b: constant byte variable
  * @n: # of bytes variable
