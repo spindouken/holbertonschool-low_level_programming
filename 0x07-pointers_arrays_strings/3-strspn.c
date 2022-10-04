c8dfcb9b49d3bec7c8df128eaa2036801660552d
spindouken <spindouken@gmail.com>
2022-10-04 12:42:53 -0500

    betty compliance

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -4,7 +4,7 @@
  * _strspn - find length of prefix substring
  * @s: string to be checked
  * @accept: substring with bytes to be matched
- * Return: number of bytes matched 
+ * Return: number of bytes matched
  */
 
 unsigned int _strspn(char *s, char *accept)
@@ -16,13 +16,15 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (y = 0; accept[y] != '\0'; y++)
 		{
-				if (s[x] == accept[y])
-				{
-					z++;
-					break;
-				}
-				if (accept[y + 1] == '\0')
-					return (z);
+			if (s[x] == accept[y])
+			{
+				z++;
+				break;
+			}
+			if (accept[y + 1] == '\0')
+			{
+				return (z);
+			}
 		}
 	}
 	return (z);
