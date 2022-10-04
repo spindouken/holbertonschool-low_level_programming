ee01445067d71e4306584ff47ae8a8700f7f3c12
spindouken <spindouken@gmail.com>
2022-10-04 12:39:47 -0500

    working code for betty check

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,7 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_chessboard - form array to print chessboard
+ * @a: array as chessboard template
  */
+
+void print_chessboard(char (*a)[8])
+{
+	int x;
+	int y;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (j = 0; j < 8; j++);
+		{
+		_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
