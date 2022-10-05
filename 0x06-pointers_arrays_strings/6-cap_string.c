5a65365b912d35a68293f80db2745a091558c7c0
spindouken <spindouken@gmail.com>
2022-10-04 20:18:10 -0500

    capitalize

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,9 +1,29 @@
 #include "main.h"
 
 /**
- * cap_string - capitalize all words of a string
- * @
- * Return: 
+ * cap_string - capitalize first letter of each word of a string
+ * @s: string to be capitalized
+ * Return: the string post capitalization 
  */
 
-char *cap_string(char *)
+char *cap_string(char *s)
+{
+	int x = 0;
+	int y;
+	nspcchar = 13;
+	char spcchar[] = " \t\n,;.!?\"(){}";
+
+	while (s[x])
+	{
+		y = 0;
+
+		while (y < nspcchar)
+		{
+			if ((x == 0 || s[x - 1] == spc[x]) && (s[x] >= 97 && s[x] <= 122))
+				s[x] -= 32;
+			y++;
+		}
+		x++;
+	}
+	return (s);
+}
