55631c7ab40139c0ab8fb5eda8c6734f8c599c06
spindouken <spindouken@gmail.com>
2022-10-04 20:57:24 -0500

    1337 tip toe

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,9 +1,25 @@
 #include "main.h"
 
 /**
- * main - check the code
- * @
- * Return: 
+ * leet - make 1337
+ * @s: string to become 1337
+ * Return: tip toe 4
  */
 
-char *leet(char *)
+char *leet(char *s)
+{
+	int x[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int y[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+
+	int a, b;
+
+	for (b = 0; s[b] != '\0'; b++)
+	{
+		for (a = 0; x[a] != '\0'; a++)
+		{
+			if (s[b] == x[a])
+				s[b] = y[a];
+		}
+	}
+	return (s);
+}
