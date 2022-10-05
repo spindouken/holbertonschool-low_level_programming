7c68346b6a9fa3bb55d7daef8c79d880736dd05e
spindouken <spindouken@gmail.com>
2022-10-04 20:22:06 -0500

    final

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -3,14 +3,14 @@
 /**
  * cap_string - capitalize first letter of each word of a string
  * @s: string to be capitalized
- * Return: the string post capitalization 
+ * Return: the string post capitalization
  */
 
 char *cap_string(char *s)
 {
 	int x = 0;
 	int y;
-	nspcchar = 13;
+	int nspcchar = 13;
 	char spcchar[] = " \t\n,;.!?\"(){}";
 
 	while (s[x])
@@ -19,7 +19,7 @@ char *cap_string(char *s)
 
 		while (y < nspcchar)
 		{
-			if ((x == 0 || s[x - 1] == spc[x]) && (s[x] >= 97 && s[x] <= 122))
+			if ((x == 0 || s[x - 1] == spcchar[x]) && (s[x] >= 97 && s[x] <= 122))
 				s[x] -= 32;
 			y++;
 		}
