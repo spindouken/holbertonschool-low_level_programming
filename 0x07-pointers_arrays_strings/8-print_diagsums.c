5c748dea5623ae05ef99f4d11b2237d74353ca43
spindouken <spindouken@gmail.com>
2022-10-05 22:08:34 -0500

    working code

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -2,12 +2,21 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * print_diagsums - print diagonal sum of an array
+ * @a: source of array
+ * @size: array size
  */
 
 void print_diagsums(int *a, int size)
 {
-	
+	int x;
+	int y = 0;
+	int z = 0;
+
+	for (x = 0; x < size; x++)
+	{
+		y = y + a[((size + 1) * x)];
+		z = z + a[((size - 1) * (x + 1))];
+	}
+	printf("%d, %d\n", y, z);
 }
