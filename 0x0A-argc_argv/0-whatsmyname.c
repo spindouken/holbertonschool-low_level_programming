b3eab7f28a47cc8ff27d5252c34b01d7be8a3d0f
spindouken <spindouken@gmail.com>
2022-10-11 15:04:53 -0500

    unclean but will try

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - have program print its own name even if changed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc)
+	
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argv[9000])
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
