589467294fbc6f7c32ec727c40a9f6ce77530775
spindouken <spindouken@gmail.com>
2022-10-11 15:13:52 -0500

    change of formate (read directions again lol)

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,11 +7,8 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc__attribute__((unused)), char *argv[])
 {
-	if (argc)
-	{}
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
