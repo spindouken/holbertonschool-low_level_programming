585e317bb9f9f9403f51220f6add2237dacff2f0
spindouken <spindouken@gmail.com>
2022-10-11 17:10:17 -0500

    lol was missing a file but program still works

---
+++ function temp/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - search string for any set of bytes
+ * @s: string to be searched
+ * @accept: bytes that are within search tearms
+ * Return: pointer to first byte occurence in s or NULL if none
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				s = &s[x];
+				return (s);
+			}
+		}
+	}
+	return ('\0');
+}
