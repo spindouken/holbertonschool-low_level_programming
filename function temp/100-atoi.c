1c3383f21c29d30f274224c4ba80baf074e40ca9
spindouken <spindouken@gmail.com>
2022-10-11 19:10:15 -0500

    update

--- function temp/100-atoi.c
+++ function temp/100-atoi.c
@@ -1,1 +1,35 @@
-int _atoi(char *s);
+#include "main.h"
+
+/**
+ * _atoi - prints signed num from string input to SO
+ * @s: input string
+ * Return: 0 if no numbers present
+ */
+int _atoi(char *s)
+{
+    int a = 0;
+    int sign = -1;
+    int numFlag = 0;
+    int retThis = 0;
+
+    while (*(s + a) != '\0')
+    {
+        if (*(s + a) == '-')
+        {
+            sign *= -1;
+        }
+        else if (*(s + a) >= '0' && *(s + a) <= '9')
+        {
+            retThis *= 10;
+            retThis -= (*(s + a) - 48);
+            numFlag = 1;
+        }
+        else if (numFlag == 1)
+        {
+            break;
+        }
+        a++;
+    }
+    retThis = (retThis * sign);
+    return (retThis);
+}
