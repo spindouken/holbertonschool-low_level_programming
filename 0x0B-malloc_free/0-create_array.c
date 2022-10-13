ac505ba1305f6082e24cae1c888a8045fb85ec49
spindouken <spindouken@gmail.com>
2022-10-12 22:50:37 -0500

    array done

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - create array & initialize with a char
+ * @size: array size
+ * @c: char
+ * Return: pointer to array or NULL
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int x;
+	char *array;
+
+	if (size == 0)
+		return (NULL);
+	
+	array = malloc(size * sizeof(char));
+
+	if (array == NULL)
+		return (NULL);
+
+	for (x = 0; x < size; x++)
+		array[x] = c;
+
+	return (array);
+}
