1b6124d627823b8971e4b78fe5c5876bc4e124d2
spindouken <mlc9181209@gmail.com>
2022-10-18 15:06:42 -0500

    try without bugfix

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -14,36 +14,27 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int x, y, z;
 	char *str;
-	char *dummy = malloc(1);
 
 	if (s1 == NULL)
 		x = 0;
-
 	else
-	{
 		x = 0;
 		while (s1[x])
 			x++;
-	}
 
 	if (s2 == NULL)
 		y = 0;
-
 	else
 	{
 		y = 0;
 		while (s2[y])
 			y++;
 	}
-
 	if (y > n)
 		y = n;
-
 	str = malloc(sizeof(char) * (x + y + 1));
-
 	if (str == NULL)
 		return (NULL);
-
 	for (z = 0; z < x; z++)
 		str[z] = s1[z];
 
@@ -51,8 +42,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[z + x] = s2[z];
 
 	str[x + y] = '\0';
-
-	free(dummy);
 
 	return (str);
 }
