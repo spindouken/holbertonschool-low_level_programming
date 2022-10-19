e0019089be823c226a2de5b96103e4dea0119cf2
spindouken <spindouken@gmail.com>
2022-10-18 20:19:16 -0500

    malloc 5

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	if (y > n)
 		y = n;
-	wat = malloc(4);
+	wat = malloc(5);
 	free(wat);
 	str = malloc(sizeof(char) * (x + y + 1));
 	if (str == NULL)
