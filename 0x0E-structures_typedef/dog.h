47487d9169b22122ee2c9fca251878568feeac7a
spindouken <spindouken@gmail.com>
2022-10-20 20:47:05 -0500

    added prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct - building dogetown
@@ -10,3 +11,10 @@ struct dog
 	char *owner;
 	float age;
 }
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
