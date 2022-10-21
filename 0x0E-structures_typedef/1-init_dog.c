e075d14858dcc178ae32d0aa9a6f90edfe54bcd8
spindouken <spindouken@gmail.com>
2022-10-20 21:20:14 -0500

    initialization of struct

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * init_dog - initialize variable of type struct dog
+ * @d: variable pointer to doge struct
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	while (d)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
