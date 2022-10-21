bec43dce6d145eafe03b674b595f21099375376a
spindouken <spindouken@gmail.com>
2022-10-20 21:44:09 -0500

    betty compliance woof

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,9 +15,9 @@ void print_dog(struct dog *d)
 		printf("Name: %s\n", "(nil)");
 	else
 		printf("Name: %s\n", d->name);
-	
+
 	printf("Age: %f\n", d->age);
-	
+
 	if (d->owner == NULL)
 		printf("Owner: %s\n", "(nil)");
 	else
