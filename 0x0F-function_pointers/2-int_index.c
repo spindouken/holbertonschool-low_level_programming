e0112b37a92c2181a2848e505b7c82464fa17093
spindouken <spindouken@gmail.com>
2022-10-25 12:58:24 -0500

    betty compliance

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,8 +16,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int x;
 
 	if (array == NULL || size <= 0 || cmp == NULL)
-		return(-1);
-	
+		return (-1);
+
 	for (x = 0; x < size; x++)
 		if (cmp(array[x]) > 0)
 			return (x);
