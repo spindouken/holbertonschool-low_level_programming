c8e9e3e0160bfcc360a250f70d7f187764dc4e2c
spindouken <spindouken@gmail.com>
2022-10-25 13:52:36 -0500

    op_function.c filled

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -8,6 +8,9 @@
  * Return: sum of a and b
  */
 int op_add(int a, int b)
+{
+	return (a + b);
+}
 
 
 /**
@@ -17,6 +20,9 @@ int op_add(int a, int b)
  * Return: difference of a and b
  */
 int op_sub(int a, int b)
+{
+	return (a - b);
+}
 
 /**
  * op_mul - multiply integers
@@ -25,6 +31,9 @@ int op_sub(int a, int b)
  * Return: product of a and b
  */
 int op_mul(int a, int b)
+{
+	return (a * b);
+}
 
 /**
  * op_div - divide integers
@@ -33,6 +42,9 @@ int op_mul(int a, int b)
  * Return: result of the division of a by b
  */
 int op_div(int a, int b)
+{
+	return (a / b);
+}
 
 /**
  * op_mod - modulus of one int by another int
@@ -41,3 +53,6 @@ int op_div(int a, int b)
  * Return: remainder of the division of a by b
  */
 int op_mod(int a, int b)
+{
+	return (a % b);
+}
