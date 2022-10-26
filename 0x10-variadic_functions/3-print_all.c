9175b4396dcd44d0cb777ecf6159e166a956bef5
spindouken <spindouken@gmail.com>
2022-10-25 23:07:20 -0500

    case switch print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
@@ -7,3 +8,35 @@
  */
 
 void print_all(const char * const format, ...)
+{
+	va_list betty;
+	char *delimiter = "";
+	unsigned int x = 0;
+
+	va_start(betty, format);
+	while (format && format[x])
+	{
+		switch (format[x])
+		{
+			case 'c':
+				printf("%s%c", delimiter, va_arg(betty, int));
+				break;
+			case 'i':
+				printf("%s%d", delimiter, va_arg(betty, int));
+				break;
+			case 'f':
+				printf("%s%f", delimiter, va_arg(betty, double));
+				break;
+			case 's':
+				if (betty == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s%s", delimiter, va_arg(betty, char *));
+				break;
+		}
+		delimiter = ", ";
+		x++;
+	}
+}
