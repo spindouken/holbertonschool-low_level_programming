fe72de0f77ac1bbe3354a11ec9d46842976dfe51
spindouken <spindouken@gmail.com>
2022-10-25 21:00:16 -0500

    calculator COMPLETED

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print result of functions post operation interaction
+ * @argc: num of args
+ * @argv: array of pointers to args
+ * Return: always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	char *operator = argv[2];
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (*operator != '+' && *operator != '-' && *operator != '*' && *operator != '/' && *operator != '%')
+	{
+		  printf("Error\n");
+		exit(99);
+	}
+	printf("%d\n", get_op_func(argv[2])((atoi(argv[1])), (atoi(argv[3]))));
+	
+	return (0);
+}
