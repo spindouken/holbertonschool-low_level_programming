7970fd4deb7157a603d57fb9d30026998aeb9cb7
spindouken <spindouken@gmail.com>
2022-11-03 16:27:50 -0500

    working code

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,14 +9,16 @@
 
 size_t print_list(const list_t *h)
 {
+	size_t x = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
-			printf
-		nodes++;
+			printf("[%d] %s\n", h->len, h->str);
+		x++;
+		h = h->next;
 	}
-	return (nodes);
+	return (x);
 }
