99cfeaa2b24a7fb3195d2a17f24339b6058b5312
spindouken <spindouken@gmail.com>
2022-11-03 18:35:43 -0500

    working code retry

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,12 +3,18 @@
 
 /**
  * list_len - function that returns the number of elements in a linked list
- * @h: pointer to list_t struct
+ * @h: pointer to list_t list
  * Return: the number of elements in linked list
 */
 
 size_t list_len(const list_t *h)
 {
+	size_t elements = 0;
 
+	while (h != NULL)
+	{
+	h = h->next;
+	elements++;
+	}
 	return (elements);
 }
