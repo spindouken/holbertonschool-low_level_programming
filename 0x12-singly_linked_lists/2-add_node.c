10e15a21a1d6b233131e947d5d46612d5149c409
spindouken <spindouken@gmail.com>
2022-11-03 19:48:30 -0500

    maybe betty knows why critical failure

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,13 +1,37 @@
 #include "lists.h"
 #include <stdio.h>
+#include <string.h>
 
 /**
  * add_node - function that adds a new node to the head of list_t
- * @h: pointer to list_t struct
+ * @head: pointer to list_t list head
+ * @str: string to add to list
  * Return: the address of the new element, or NULL if it failed
 */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	return()
+	list_t *node;
+	int len = 0;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+
+	while (str[len] != '\0')
+		len++;
+
+	while (node != NULL)
+	{
+		node->str = strdup(str);
+		if (node->str == NULL)
+		{
+			free(node);
+			return(NULL);
+		}
+		node->len = len;
+		node->next = *head;
+	}
+	*head = node;
+	return(node);
 }
