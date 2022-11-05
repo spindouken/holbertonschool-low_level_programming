6068bb3a8c1e8124ba01b17982e41bb54f713bb7
spindouken <spindouken@gmail.com>
2022-11-05 18:17:07 -0500

    massive local push

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,1 +1,26 @@
+#include "lists.h"
+
+/**
+ * insert_nodeint_at_index - function that returns the sum of all the data (n) of a listint_t linked list
+ * @head: pointer to the address of head of list_t
+ * @idx: index of the list where the new node should be added
+ * @n: integer data of new node
+ * Return: address of the new node, or NULL on failure
+ */
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	list_t *node;
+
+	node = malloc(sizeof(listint_t));
+	if (node == NULL)
+		return (NULL);
+
+	if (node != NULL)
+	{
+		node->n = n;
+		node->next = *head;
+	}
+	*head = node;
+	return (node);
+}
