3a42bf5a87046c6db7f6170d0aab84ae3616b673
spindouken <spindouken@gmail.com>
2022-11-06 18:17:43 -0600

    betty compliance

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,19 +12,19 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int conv = 0;
 	int x = 0;
-		
+
 	if (b == NULL)
-		return 0;
+		return (0);
 
 	while (b[x] != '\0')
 	{
 		if (b[x] != '0' && b[x] != '1')
 			return (0);
-		
+
 		while (b[x] == '0' || b[x] == '1')
 		{
 			conv <<= 1;
-			conv += (b[x]-('0'));
+			conv += (b[x] - ('0'));
 			x++;
 		}
 	}
