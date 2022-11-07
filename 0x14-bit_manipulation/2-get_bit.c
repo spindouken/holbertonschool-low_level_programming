f14a297ea6a35816d93d8e54b29a49341d494781
spindouken <spindouken@gmail.com>
2022-11-06 18:21:33 -0600

    betty compliance

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /* get_bit - function that returns the value of a bit at a given index
- * @n:
+ * @n: number to be printed in binary
  * @index:
  * Return: the value of the bit at index or -1 if an error occured
 */
