51bfc4b86ab3b37427ee7648881025234cd25008
spindouken <spindouken@gmail.com>
2022-11-06 18:19:21 -0600

    betty compliance

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/* print_binary - function that prints the binary representation of a number
+/* 
+ * print_binary - function that prints the binary representation of a number
  * @n: number to be printed in binary
  * Return: void
 */
@@ -9,6 +10,6 @@ void print_binary(unsigned long int n)
 {
 	if (n > 1)
 		print_binary(n >> 1);
-	
+
 	_putchar((n & 1) + '0');
 }
