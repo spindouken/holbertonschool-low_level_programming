f12c51da09e5b3f1a67f21a8b160b2621ba2ab3c
spindouken <mlc9181209@gmail.com>
2022-11-08 17:06:39 -0600

    betty pls

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,8 +3,8 @@
 /**
  * create_file - creates a file
  * @filename: text file name
- * @text_file: NULL terminated string to write to the file
- * Return: 1 on success, -1 on failure (file can not be created, 
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure (file can not be created,
  * file can not be written, write “fails”, etc…)
  * if filename is NULL return -1
  */
@@ -16,14 +16,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[length] != NULL) 
+	while (text_content[length] != NULL)
 		length++;
 
 	newfile = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (newfile == -1)
 		return (-1);
-	
+
 	if (text_content == NULL)
 	{
 		close(newfile);
