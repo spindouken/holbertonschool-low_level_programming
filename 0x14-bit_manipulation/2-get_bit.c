fc0f42f61bdae24b9a1dab9dec7ef3860745d61c
spindouken <spindouken@gmail.com>
2022-11-07 21:08:23 -0600

    betty compliance

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,7 +4,7 @@
  * @n: number to be printed in binary
  * @index:
  * Return: the value of the bit at index or -1 if an error occured
-*/
+ */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
