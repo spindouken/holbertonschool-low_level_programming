5ed709c47eb031f67e4c205fc8ab196d8e9dd269
spindouken <spindouken@gmail.com>
2022-11-07 20:36:49 -0600

    big push for check

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,10 +3,16 @@
 /*
  * flip_bits - function that returns the number of bits you would
  * need to flip to get from one number to another
- * Return: ^
+ * @n:
+ * @m: 
+ * Return: see function description
 */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned int long bitcount = 0;
 
+	bitcount += ((n ^ m) & 1);
+
+	return (bitcount);
 }
--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,5 +12,26 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int targetfile, readfile, writefile;
+	char *filetext;
 
+
+	if (filename == NULL)
+		return (0);
+	filetext = malloc(letters);
+	targetfile = open(filename, O_RDONLY);
+	if (targetfile == -1)
+		return (0);
+	if (filetext == NULL)
+		return (0);
+	readfile = read(targetfile, filetext, letters);
+	if (readfile == -1)
+		return (0);
+	writefile = write(STDOUT_FILENO, filetext, readfile);
+	if (writefile == -1)
+		return (0);
+
+	free(filetext);
+	close(targetfile);
+	return (writefile);
 }
