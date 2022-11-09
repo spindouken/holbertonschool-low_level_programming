154e31313c0374aba5d29799b79246a3199e18c0
spindouken <spindouken@gmail.com>
2022-11-08 19:06:03 -0600

    betty compliance

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at_index - function that returns the sum of all the data (n) of a listint_t linked list
+ * insert_nodeint_at_index - function that returns the sum of
+ * all the data (n) of a listint_t linked list
  * @head: pointer to the address of head of list_t
  * @idx: index of the list where the new node should be added
  * @n: integer data of new node
@@ -29,10 +30,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	for (nodecounter = 0; nodecounter < (idx - 1); nodecounter++)
 	{
-		if(!(temp) || (temp->next))
+		if (!(temp) || (temp->next))
 			return (NULL);
 	}
 	node->next = temp->next;
-	temp->next = node; 
+	temp->next = node;
 	return (node);
 }
