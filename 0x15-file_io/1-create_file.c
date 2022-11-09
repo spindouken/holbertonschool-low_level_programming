2e8c15d729ddc510a704089a4910660574bf6822
spindouken <spindouken@gmail.com>
2022-11-08 19:25:46 -0600

    rework

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	while (text_content[length])
-		length++;
+	if (text_content)
+		while (text_content[length])
+			length++;
 
 	newfile = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 
