217068ab0c9c6c26d92ea76e5f7a6dbaf62831c2
spindouken <spindouken@gmail.com>
2022-11-08 18:47:39 -0600

    2 proto

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,22 +1,18 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
+ * append_text_to_file - appends text to the end of a file
+ * @filename: text file name
+ * @text_content: NULL terminated string to write to the file
+ * Return: 1 on success 
+ * -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ * if filename is NULL return -1
+ * Return 1 if the file already exists
+ * Return -1 if the file does not exist or do not have permissions to write
  */
-int main(int ac, char **av)
-{
-    int res;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: %s filename text\n", av[0]);
-        exit(1);
-    }
-    res = append_text_to_file(av[1], av[2]);
-    printf("-> %i)\n", res);
-    return (0);
+int append_text_to_file(const char *filename, char *text_content)
+{
+	
 }
