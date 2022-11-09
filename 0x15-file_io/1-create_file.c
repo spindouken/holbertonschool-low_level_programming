bb8328babd60f257d6301501354ea32d2d1ced3f
spindouken <spindouken@gmail.com>
2022-11-08 19:28:57 -0600

    edge case

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,6 +12,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int newfile, length = 0;
+	int writevalue;
 
 	if (filename == NULL)
 		return (-1);
@@ -34,6 +35,10 @@ int create_file(const char *filename, char *text_content)
 	}
 	else
 		write(newfile, text_content, length);
+		
+	writevalue = write(newfile, text_content, length);
+	if (writevalue < 0)
+		return (-1);
 
 	close(newfile);
 	return (1);
