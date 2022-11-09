c509773fc8049ee4123cb43ce0ae80798ea511c7
spindouken <spindouken@gmail.com>
2022-11-08 19:10:20 -0600

    resend.. changes broke code

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,10 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int bitcount = 0;
 
-	while ((n ^ m) > 0)
-	{
-		bitcount += ((n ^ m) & 1);
-		(n ^ m) >>= 1;
-	}
+	bitcount += ((n ^ m) & 1);
+
 	return (bitcount);
 }
