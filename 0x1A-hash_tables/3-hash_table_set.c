36cf941754b2b430dab727d9a3f3a912c81f6ca3
spindouken <spindouken@gmail.com>
2022-11-15 18:59:46 -0600

    big local push

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,5 +10,33 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *node, *copy;
 
+	if (ht == NULL || key == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	copy = ht->array[index];
+	while (copy != NULL)
+	{
+		if (strcmp(copy->key, key) == 0)
+		{
+			free(copy->value);
+			copy->value = strdup(value);
+			if (copy->value == NULL)
+				return (0);
+			return (1);
+		}
+		copy = copy->next;
+	}
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	if (node->value == NULL)
+		return (0);
+	node->next = ht->array[index];
+	ht->array[index] = node;
+	return (1);
 }
