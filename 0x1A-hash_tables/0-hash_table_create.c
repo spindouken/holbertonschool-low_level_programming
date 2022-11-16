b9c18730c78fd1facb4e3a8df925ddaaa7151c57
spindouken <spindouken@gmail.com>
2022-11-15 19:09:00 -0600

    forgot initialization

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *firsthashtable;
+	unsigned long int x;
 
 	firsthashtable = malloc(sizeof(hash_table_t));
 	if (firsthashtable == NULL)
